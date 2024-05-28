392bff150ac8059219267d2e479ca9a61dd6c244
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2024-05-28 12:21:06 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -10,50 +10,53 @@ async function submitSendForm(times = 1, time = 0, timeBetween = 0) {
     let sendButton = document.querySelector("*[aria-label=Senden]")
 
     if (!sendButton) {
-        alert("the sendbutton have to be vidible | button not found")
+        alert("The send button must be visible | button not found");
+        return; // Exit the function if the button is not found
     }
 
     if (time > 0) {
-        await wait(time)
+        await wait(time);
     }
 
     for (let index = 0; index < times; index++) {
         if (timeBetween > 0) {
-            await wait(timeBetween)
+            await wait(timeBetween);
         }
-        sendButton.click()
-        console.log("round", index)
+        sendButton.click();
+        console.log("round", index + 1);
     }
 }
 
-//form
-
-const submitForm = document.createElement("form")
-submitForm.classList.add("bot-form")
-submitForm.classList.add("invisible")
+// Form
+const submitForm = document.createElement("form");
+submitForm.classList.add("bot-form");
+submitForm.classList.add("invisible");
 
 submitForm.innerHTML = `
-<input type="number" name="times" placeholder="Times" />
-<input type="number" name="time " step="0.001" placeholder="Time (minutes)" />
-<input type="number" name="timeBetween" step="0.001" placeholder="Time Between (minutes)" />
-<button>Run</button>
-`
+    <input type="number" name="times" placeholder="Times" />
+    <input type="number" name="time" step="0.001" placeholder="Time (minutes)" />
+    <input type="number" name="timeBetween" step="0.001" placeholder="Time Between (minutes)" />
+    <button>Run</button>
+`;
 
 submitForm.onsubmit = e => {
-    e.preventDefault()
-    const fd = new FormData(e.target)
-    submitSendForm(parseFloat(fd.get("times")), parseFloat(fd.get("time")), parseFloat())
-    submitForm.classList.toggle("invisible")
+    e.preventDefault();
+    const fd = new FormData(e.target);
+    const times = parseFloat(fd.get("times"));
+    const time = parseFloat(fd.get("time"));
+    const timeBetween = parseFloat(fd.get("timeBetween"));
+
+    submitSendForm(times, time, timeBetween);
+    submitForm.classList.toggle("invisible");
 }
 
-document.documentElement.appendChild(submitForm)
+document.documentElement.appendChild(submitForm);
 
-//button
+// Button
+const botButton = document.createElement("button");
+botButton.innerText = "WhatsApp Bot";
+botButton.classList.add("bot-button");
 
-const botButton = document.createElement("button")
-botButton.innerText = "Whatsapp Bot"
-botButton.classList.add("bot-button")
+botButton.onclick = () => submitForm.classList.toggle("invisible");
 
-botButton.onclick = () => submitForm.classList.toggle("invisible")
-
-document.documentElement.appendChild(botButton)
+document.documentElement.appendChild(botButton);
